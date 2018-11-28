e8905251a3bbf498d0b7b904c5f92df3bb0d8b44
Tianhao Zhou <tianhaoz@umich.edu>
2018-11-28 16:24:26 +0000

    fix style and show dataset brief

--- dataset.py
+++ dataset.py
@@ -1,15 +1,17 @@
 import os
 import pandas as pd
 
+
 class Dataset():
     def __init__(self):
         self.raw_dataset = {}
-    
+
     def load_raw_dataset(self, dataset_root, dataset_meta, dataset_id):
-        abs_dataset_filename = os.path.join(dataset_root, dataset_meta[dataset_id]['filename'])
+        dataset_filename = dataset_meta[dataset_id]['filename']
+        abs_dataset_filename = os.path.join(dataset_root, dataset_filename)
         print('Loading ' + dataset_id + ' from ' + abs_dataset_filename + '...')
         raw_dataset_content = pd.read_csv(abs_dataset_filename)
         self.raw_dataset[dataset_id] = raw_dataset_content
-        
+
     def show_raw_brief(self, dataset_meta, dataset_id):
         self.raw_dataset[dataset_id].head(5)
--- util.py
+++ util.py
@@ -1,8 +1,11 @@
 import config
 import dataset
 
+
 def load_dataset(dataset_root):
     res = dataset.Dataset()
     res.load_raw_dataset(dataset_root, config.dataset_meta, 'train')
+    res.show_raw_brief(config.dataset_meta, 'train')
     res.load_raw_dataset(dataset_root, config.dataset_meta, 'test')
+    res.show_raw_brief(config.dataset_meta, 'test')
     return res
