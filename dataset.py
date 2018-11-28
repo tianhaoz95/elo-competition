89ff30e9457be07b68f5e920881a0e27db10124d
tianhaoz95 <tianhaoz@umich.edu>
2018-11-27 23:13:04 -0800

    minor fix

--- dataset.py
+++ dataset.py
@@ -5,7 +5,8 @@ class Dataset():
     def __init__(self):
         self.raw_dataset = {}
     
-    def load_raw_dataset(self, dataset_root, dataset_meta):
-        abs_train_dataset_filename = os.path.join(dataset_root, dataset_meta['train']['filename'])
-        raw_train_dataset_content = pd.read_csv(abs_train_dataset_filename)
-        self.raw_dataset['train'] = raw_train_dataset_content
+    def load_raw_dataset(self, dataset_root, dataset_meta, dataset_id):
+        abs_dataset_filename = os.path.join(dataset_root, dataset_meta[dataset_id]['filename'])
+        print('Loading ' + dataset_id + ' from ' + abs_dataset_filename + '...')
+        raw_dataset_content = pd.read_csv(abs_dataset_filename)
+        self.raw_dataset[dataset_id] = raw_dataset_content
--- util.py
+++ util.py
@@ -3,5 +3,6 @@ import dataset
 
 def load_dataset(dataset_root):
     res = dataset.Dataset()
-    res.load_raw_dataset(dataset_root, config.dataset_meta)
+    res.load_raw_dataset(dataset_root, config.dataset_meta, 'train')
+    res.load_raw_dataset(dataset_root, config.dataset_meta, 'test')
     return res
