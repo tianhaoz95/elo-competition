85d772c351219304651fa9b5dfe652fbb502f9bc
tianhaoz95 <tianhaoz@umich.edu>
2018-11-27 20:11:57 -0800

    add dataset download utilities

--- util.py
+++ util.py
@@ -1,2 +1,18 @@
+import os
+import urllib
+
+def create_dir_if_not_exist(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def download_if_not_exist(root_dir, url, filename):
+    abs_filename = os.path.join(root_dir, filename)
+    create_dir_if_not_exist(root_dir)
+    if not os.path.isfile(abs_filename):
+        urllib.urlretrieve(url, abs_filename)
+
+def download_dataset():
+    print('Downloading dataset ...')
+
 def load_dataset():
-    print("Loading dataset ...")
+    print('Loading dataset ...')
