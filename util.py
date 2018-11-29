3509e8b30dd05a6889a82be86d44af9f85869388
tianhaoz95 <tianhaoz@umich.edu>
2018-11-28 21:52:53 -0800

    Update util.py

--- util.py
+++ util.py
@@ -8,3 +8,5 @@ def common_routine(dataset_root):
     dataset_object.show_raw_brief(config.dataset_meta, 'train')
     dataset_object.load_raw_dataset(dataset_root, config.dataset_meta, 'test')
     dataset_object.show_raw_brief(config.dataset_meta, 'test')
+    batch = dataset_object.get_raw_batch(10, 0, ['feature_1', 'feature_2'], training=True)
+    print(batch)
