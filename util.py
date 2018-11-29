100c1ddf870a223f48c847d0b163d61e4bc319f5
tianhaoz95 <tianhaoz@umich.edu>
2018-11-28 21:43:12 -0800

    change function name

--- util.py
+++ util.py
@@ -2,7 +2,7 @@ import config
 import dataset
 
 
-def load_dataset(dataset_root):
+def common_routine(dataset_root):
     res = dataset.Dataset()
     res.load_raw_dataset(dataset_root, config.dataset_meta, 'train')
     res.show_raw_brief(config.dataset_meta, 'train')
