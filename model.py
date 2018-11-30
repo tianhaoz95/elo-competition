659a683cdb321af47cd618d59866a4d9e5ffbd13
Tianhao Zhou <tianhaoz@umich.edu>
2018-11-29 22:33:51 -0800

    added model class

---
+++ model.py
@@ -0,0 +1,21 @@
+class Model():
+    def __init__(self):
+        pass
+    
+    def get_feature_ids(self):
+        pass
+
+    def preprocess_raw_feature(self):
+        pass
+    
+    def preprocess_batch(self):
+        pass
+
+    def generate_model(self):
+        pass
+
+    def train(self):
+        pass
+    
+    def test(self):
+        pass
